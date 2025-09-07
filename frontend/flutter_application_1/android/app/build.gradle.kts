88912da3376a6588fbe8187559227b95fe7ad319
= <ardhiraffi0@gmail.com>
2025-09-08 04:40:07 +0700

    Upload build apk

--- frontend/flutter_application_1/android/app/build.gradle.kts
+++ frontend/flutter_application_1/android/app/build.gradle.kts
@@ -2,27 +2,20 @@ plugins {
     id("com.android.application")
     id("kotlin-android")
     id("com.google.gms.google-services")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
 dependencies {
-    // Firebase BoM memastikan semua versi Firebase kompatibel
     implementation(platform("com.google.firebase:firebase-bom:34.0.0"))
-
-    // Tambahkan Firebase product yang ingin digunakan
-    implementation("com.google.firebase:firebase-auth") // untuk login
-    implementation("com.google.android.gms:play-services-auth") // untuk Google Sign-In
-
-    // Kalau kamu butuh Analytics juga, tambahkan:
-    // implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.android.gms:play-services-auth")
 }
-
 
 android {
     namespace = "com.example.flutter_application_1"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    // Perbaikan: Ganti ndkVersion agar sesuai dengan plugin
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -34,12 +27,9 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        // applicationId = "com.example.flutter_application_1"
         applicationId = "com.insightspark.perisai"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        // Perbaikan: Ubah minSdk menjadi 23 agar kompatibel dengan Firebase
+        minSdk = 23
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
@@ -47,8 +37,6 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
